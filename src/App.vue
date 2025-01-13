c3bdfc3e1373ea5ef57432a7442fc1994b13b92a
cxy13h <2546836973@qq.com>
2025-01-13 14:12:17 +0800

    first upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Tree from "./components/Tree.vue"
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <Tree/>
+  </main>
+</template>
+
+<style scoped>
+</style>
